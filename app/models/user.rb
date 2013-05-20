d149a9a39d75bdc8c7b5dbff35799f5e550d1fa2
David Lesches <david@lesches.com>
2013-05-20 10:56:10 -0400

    readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
 
+  # There is a gotcha with paperclip. It sticks images in a regular_users/ directory.
+  # This seems to fix it but test to ensure it isn't saving all images. Ie when a user updates
+  # his image it should by default delete the old one and I think with this fix that does not happen
+  #   has_attached_file :photo,
+  #                   :styles => { :medium => "300x300#", :small => "100x100#", :thumb => "50x50#" },
+  #                   :url => "/system/users/:attachment/:id/:style/:basename.:extension"
+
   # Associations
   has_many :accounts, :dependent => :destroy
 
